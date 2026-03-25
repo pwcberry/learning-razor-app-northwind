4dba11e029a75d6a6f2fe9bbeec2983b977c8fa0
Peter Berry <pberry@australianunity.com.au>
2026-03-25 16:54:34 +1100

    Update README

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ dotnet user-secrets init --project ./data/RazorApp.Data.csproj
 dotnet user-secrets set "ConnectionString" $PATH_TO_SQLITE_FILE --project .\data\RazorApp.Data.csproj
 ```
 
+Specifying the data source for a local database as a user secret is more convenient than using an environment variable, unless Docker is involved.
+
 ## References
 
 * [Safe storage of app secrets in development in ASP.NET Core](https://learn.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-10.0)
---
+++ data/Models/ProductDetail.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorApp.Data.Models;
+
+[Table("ProductDetailsV")]
+public partial class ProductDetail
+{
+    public int? Id { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string? QuantityPerUnit { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
+    public int? UnitsInStock { get; set; }
+
+    public int? UnitsOnOrder { get; set; }
+
+    public int? ReorderLevel { get; set; }
+
+    public int? Discontinued { get; set; }
+
+    public string? CategoryName { get; set; }
+
+    public string? CategoryDescription { get; set; }
+
+    public string? SupplierName { get; set; }
+
+    public string? SupplierRegion { get; set; }
+}
